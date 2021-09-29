0ba4d54cd737835d2d6df633a765006ac39e8423
gustavorr <gustavo@constata.io>
2021-09-29 15:26:16 -0300

    form

--- src/app/inscricao/inscricao.component.ts
+++ src/app/inscricao/inscricao.component.ts
@@ -8,20 +8,50 @@ import { FormBuilder, FormGroup,Validators  } from '@angular/forms';
 })
 export class InscricaoComponent implements OnInit {
   FormGroup: FormGroup;
+  FormDadosPessoais: FormGroup;
   constructor(private formBuilder: FormBuilder) {
     this.FormGroup = this.formBuilder.group({
-      // pais: ['', Validators.required],
-      // projeto: ['', Validators.required],
-      // data: ['',  Validators.required],
-      // qtdSemana: ['',  Validators.required]
+       pais: ['', Validators.required],
+       projeto: ['', Validators.required],
+       data: ['',  Validators.required],
+       qtdSemana: ['',  Validators.required]
   });
+
+  this.FormDadosPessoais = this.formBuilder.group({
+     nome: ['', Validators.required],
+     sexo: ['', Validators.required],
+     cpf: ['',  Validators.required],
+     dataNascimento: ['', Validators.required],
+     cel: ['',  Validators.required],
+     telefone: ['',  Validators.required],
+     email: ['',  Validators.required],
+     profissao: ['',  Validators.required],
+     instagram: ['',  Validators.required],
+     tamanhoCamiseta: ['',  Validators.required],
+     estadoCivil: ['',  Validators.required],
+     ficouSabendo: ['',  Validators.required],
+     nascionalidade: ['',  Validators.required],
+     passaporte: ['',  Validators.required],
+     validadePassaporte: ['',  Validators.required],
+     paisPassaporte: ['',  Validators.required],
+     cep: ['',  Validators.required],
+     cidadeEndereco: ['',  Validators.required],
+     endereco: ['',  Validators.required],
+     numeroEndereco: ['',  Validators.required],
+     bairroEndereco: ['',  Validators.required],
+     estadoEndereco: ['',  Validators.required],
+     paisEndereco: ['',  Validators.required],
+     complementoEndereco: ['',  Validators.required],
+     nomeContatoEmergencia: ['',  Validators.required],
+     parentescoContatoEmergencia: ['',  Validators.required],
+     celularContatoEmergencia: ['',  Validators.required],
+});
   }
 
   ngOnInit(): void {
     debugger;
   }
   postar() {
-    debugger;
     if (!this.FormGroup.valid) {
       console.log("Formulário inválido");
       return;
